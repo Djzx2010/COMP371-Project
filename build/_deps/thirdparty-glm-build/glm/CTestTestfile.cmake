49d71c9250bef661239e221245282ba18f5724ba
Adam Richard <60020849+arich94@users.noreply.github.com>
2021-02-17 08:18:06 -0500

    Delete build directory
    This folder should not be part of our repository, and can cause compiler errors when trying to build the project on a different machine. will also update gitignore so that this directory is never added. Please be sure not to commit your buil folder. Thanks!

--- build/_deps/thirdparty-glm-build/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/STRIX/Desktop/School/University/Winter 2021/COMP 371 - Computer Graphics/Assignment/build/_deps/thirdparty-glm-src/glm
-# Build directory: C:/Users/STRIX/Desktop/School/University/Winter 2021/COMP 371 - Computer Graphics/Assignment/build/_deps/thirdparty-glm-build/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
